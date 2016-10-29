7a52ee57ab756a37425fdc4c596869ea6a704c16
Esteban <eginez@gmail.com>
2016-10-28 22:29:56 -0700

    adding some music with patterns and ring composition

--- src/scratch-2.rb
+++ src/scratch-2.rb
@@ -1,19 +1,14 @@
 synth_cyc="#{File.expand_path "~"}/Documents/synth_cycles"
+cyc_patt="#{File.expand_path "~"}/Documents/CyclicPatterns_Mini_Sp"
+
 load_sample synth_cyc
+load_sample cyc_patt
 
-live_loop :s1 do
-  sample synth_cyc, "Breaking", amp: 3
-  with_fx :reverb do
-      #sample synth_cyc, "Resonance", beat_stretch: 8
-      sample synth_cyc, "Cuchillo", amp: 0.5
-  end
-    sleep 8
-end
-
-
-live_loop :bass  do
+live_loop :ack do
   tick
-  pattern = ring 1,0,0,0,1,0,1,1
-  sample :bd_haus, amp: 4 if pattern.look == 1
-  sleep 0.25
+  change  = (ring [0]*13 + [5]*10+ 4*[7])
+  with_fx :reverb, reps: 32, room: 1 do
+    synth :tb303, cutoff: rrand(70, 120), note: (ring :e1, :e2, :e3).tick + 12 - change.look ,  release: 0.1
+  end
+  sleep 0.15
 end
